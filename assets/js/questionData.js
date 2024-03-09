3af53430517f1131f4f57beda5de0ebd8fb637eb
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-03-09 13:52:40 -0500

    made each questoin unique, created element selector ln 8,  crated setInterval, time remaining variables, created a global variable and set the index to 0 (ln12), created function for start timer, render questions (ln 21 and put a for loop then appendchild to button and li. Created function on ln 33; startTimer, Created function on ln 36 & added event listener.

--- assets/js/questionData.js
+++ assets/js/questionData.js
@@ -5,22 +5,22 @@ questionData=[
         solution: 'C2'
     },
     {
-        title: 'Q1',
+        title: 'Q2',
         choices: ['C1', 'C2', 'C3', 'C4'],
         solution: 'C1'
     },
     {
-        title: 'Q1',
+        title: 'Q3',
         choices: ['C1', 'C2', 'C3', 'C4'],
         solution: 'C4'
     },
     {
-        title: 'Q1',
+        title: 'Q4',
         choices: ['C1', 'C2', 'C3', 'C4'],
         solution: 'C3'
     },
     {
-        title: 'Q1',
+        title: 'Q5',
         choices: ['C1', 'C2', 'C3', 'C4'],
         solution: 'C1'
     },
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,36 @@ var highscoreEl= document.getElementById("highscore")
 var startQuizEl = document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
+var timerEl=document.getElementById("timer")
+
+var setIntervalId
+var timeRemaining=questionData.length*15
+var index=0
 
 function startQuiz(){
-    introEl.classList.add("hide")
-    questionSectionEl.classList.remove("hide")
+    introEl.classList.add('hide')
+    questionSectionEl.classList.remove('hide')
+
+    setIntervalId=setInterval(startTimer , 1000)
+    renderQuestions()
+}
+function renderQuestions(){
+    questionTitleEl.textContent=questionData[index].title
+    choiceListEl.textContent=""
+    for(var i=0; i<questionData[index].choices.length; i++){
+        var li=document.createElement("li")
+        var button=document.createElement("button")
+        button.textContent=questionData[index].choices[i]
+
+        li.appendChild(button)
+        choiceListEl.appendChild(li)
+    }
+}
+function startTimer(){
+    timerEl.textContent=timeRemaining--
+}
+function nextQuestion(){
 
 }
-
 startQuizEl.addEventListener("click", startQuiz) //call back
+choiceListEl.addEventListener("click", nextQuestion)
