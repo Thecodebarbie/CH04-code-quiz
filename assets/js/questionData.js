58cb5fbe29140888212aae59d7dd3479561fa941
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-03-09 11:05:14 -0500

    DEBUG typescript error

--- assets/js/questionData.js
+++ assets/js/questionData.js
@@ -1,27 +1,27 @@
-questionData = [
+questionData=[
     {
-        title:"Q1",
-        choices:["C1", "C2", "C3", "C4"]
-        solution:"C3"
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C2'
     },
     {
-        title:"Q2",
-        choices:["C1", "C2", "C3", "C4"]
-        solution:"C1"
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C1'
     },
     {
-        title:"Q3",
-        choices:["C1", "C2", "C3", "C4"]
-        solution:"C4"
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C4'
     },
     {
-        title:"Q4",
-        choices:["C1", "C2", "C3", "C4"]
-        solution:"C2"
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C3'
     },
     {
-        title:"Q5",
-        choices:["C1", "C2", "C3", "C4"]
-        solution:"C2"
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C1'
     },
 ]
