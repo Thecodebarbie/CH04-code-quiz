45718f1748a70539ca94299bc073856c1c70f580
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-06-01 10:08:44 -0400

    Revert "In this commit, I've Added new coding quiz questions to questionData array and updated background color"
    This reverts commit 2dd60ad5dc693d520bac7191226a06904f6a0208.

--- assets/js/questionData.js
+++ assets/js/questionData.js
@@ -1,27 +1,27 @@
 questionData=[
     {
-        title: 'What does CSS stand for?',
-        choices: ['Creative Style Sheets', 'Cascading Style Sheets', 'Computer Style Sheets', 'Colorful Style Sheets'],
-        solution: 'Cascading Style Sheets'
+        title: 'Q1',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C2'
     },
     {
-        title: 'Which data structure uses LIFO (Last In, First Out) principle?',
-        choices: ['Stack', 'Linked List', 'Queue', 'Tree'],
-        solution: 'Stack'
+        title: 'Q2',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C1'
     },
     {
-        title: 'What does the "git clone" command do in Git?',
-        choices: ['Creates a new branch', 'Creates a new branch', 'Deletes a repository', 'Downloads a copy of a repository from a remote source'],
-        solution: 'Downloads a copy of a repository from a remote source'
+        title: 'Q3',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C4'
     },
     {
-        title: 'In JavaScript, which method is used to add new elements to the end of an array?',
-        choices: ['add()', 'extend()', 'push()', 'append()'],
-        solution: 'push()'
+        title: 'Q4',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C3'
     },
     {
-        title: 'What does the acronym "HTTP" stand for?',
-        choices: ['Hyper Transfer Text Protocol', 'HyperText Transfer Protocol', 'High Text Transfer Protocol', ' High Transfer Text Protocol'],
-        solution: 'HyperText Transfer Protocol'
+        title: 'Q5',
+        choices: ['C1', 'C2', 'C3', 'C4'],
+        solution: 'C1'
     },
 ]
